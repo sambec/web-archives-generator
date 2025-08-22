fdc8f9ac631ba641112dff6bd013cef89dcf0c8a
sambec <sarahambec@gmail.com>
2025-08-22 17:10:09 +0200

    :fire: delete old files

--- converter.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import *
-
-# Load the mp4 file
-video = VideoFileClip("Mixtape/Z_light_academia_playlist.mp4")
-
-# Extract audio from video
-video.audio.write_audiofile("Musique/A_light_academia_playlist.mp3")
